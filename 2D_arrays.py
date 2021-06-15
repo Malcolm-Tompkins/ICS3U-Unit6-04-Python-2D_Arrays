88073e78df8983599c1c3f57117165385fc19d76
Malcolm-Tompkins <82959788+Malcolm-Tompkins@users.noreply.github.com>
2021-06-15 09:42:17 -0400

    Update 2D_arrays.py

--- 2D_arrays.py
+++ 2D_arrays.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 # Created by Malcolm Tompkins
-# Created on June 8, 2021
-# Generates random numbers and find they're average
+# Created on June 10, 2021
+# Generates random numbers and find their average
 
 import random
 
 
+def random_number_grid(rows, columns, sums):
+    # Function that finds the average of the random number grid
+    total_sum = sum(sums)
+    average = total_sum / (rows * columns)
+    return average
+
+
 def main():
+    # Function for input and output
     rows_and_columns = []
     sums = []
     user_rows = (input("Enter your amount of rows: "))
@@ -20,18 +28,17 @@ def main():
             for loop_counter_rows in range(0, rows):
                 temp_list = []
                 for loop_counter_columns in range(0, columns):
-                    a_random_number = random.randint(1, 50)
-                    temp_list.append(a_random_number)
-                    print("{0} ".format(a_random_number), end="")
-
+                    random_number = random.randint(1, 50)
+                    temp_list.append(random_number)
+                    print("{0} ".format(random_number), end="")
                 print("")
                 sum_row = sum(temp_list)
                 sums.append(sum_row)
                 rows_and_columns.append(temp_list)
-            total_sum = sum(sums)
-            average = total_sum / (rows * columns)
-            print("The sum of these numbers is: {}".format(total_sum))
-            print("The average of these numbers is: {:.2f}".format(average))
+            final_average = random_number_grid(rows, columns, sums)
+            
+            print("The average of these numbers is: {:.2f}".format
+                  (final_average))
         except Exception:
             print("{} is not an integer".format(user_columns))
     except Exception:
